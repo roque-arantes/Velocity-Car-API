2d16c2c2f65cb32302cad1fd17714ec6e2bb47ec
roque-arantes <mrarantes07@gmail.com>
2026-03-26 07:45:29 -0300

    Initial Commit

---
+++ src/main/java/br/com/fiap/velocitycar/VelocityCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.velocitycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocityCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelocityCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/velocitycar/VelocityCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.velocitycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelocityCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
